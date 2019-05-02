ade11d3b4ca2993f15a800f542b999309221ef98
Felipe Monobe <xinube@gmail.com>
2019-05-02 01:03:54 +0000

    high_scores refactoring (immutability)

--- exercises/high-scores/high_scores.py
+++ exercises/high-scores/high_scores.py
@@ -1,3 +1,12 @@
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self.scores = scores
+    
+    def personal_best(self):
+        return sorted(self.scores, reverse = True)[0]
+
+    def latest(self):
+        return self.scores[len(self.scores) - 1]
+
+    def personal_top_three(self):
+        return sorted(self.scores, reverse = True)[0:3]
