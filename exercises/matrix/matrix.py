2d128b5e4779e7139e344049ba1061ad4ccd951d
Felipe Monobe <xinube@gmail.com>
2019-05-04 15:31:54 +0000

    matrix done

--- exercises/matrix/matrix.py
+++ exercises/matrix/matrix.py
@@ -1,9 +1,12 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
 
     def row(self, index):
-        pass
+        rowString = self.matrix_string.splitlines()[index - 1]
+        return [int(x) for x in rowString.split()]
 
     def column(self, index):
-        pass
+        matrixString = [x.split() for x in self.matrix_string.splitlines()]
+        columnStringList = [x[index - 1] for x in matrixString]
+        return [int(x) for x in columnStringList]
