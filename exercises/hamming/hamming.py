5237f1da675fe0b4884e484e935df37339fe673d
Felipe Monobe <xinube@gmail.com>
2019-05-04 21:31:45 +0000

    hamming done

--- exercises/hamming/hamming.py
+++ exercises/hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+  if len(strand_a) != len(strand_b):
+    raise ValueError('DNA strands differ in length.')
+
+  distance = 0
+  for a, b in zip(strand_a, strand_b):
+    if a != b:
+      distance += 1
+  return distance
