06946615772967898d24f46955e1c9c1d15ec1ed
Felipe Monobe <xinube@gmail.com>
2019-05-11 18:25:05 -0300

    isogram done

--- exercises/isogram/isogram.py
+++ exercises/isogram/isogram.py
@@ -1,2 +1,8 @@
 def is_isogram(string):
-    pass
+  if not string: return True
+
+  treated_string = string.replace(' ', '').replace('-', '').lower()
+  for index, char in enumerate(treated_string, start = 1):
+    if char in treated_string[:index - 1]: return False
+
+  return True
